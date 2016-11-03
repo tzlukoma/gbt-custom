ab12af1ce3ead20c072e7d9252cfa41ccfce0eb5
Thomas Z Lukoma <tzlukoma@morethanahut.com>
2016-11-02 22:29:14 -0400

    MoreThanAHut Custom Theme Initial Commit
    Initial commit with proper MTAH attribution

---
+++ functions.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * Genesis Sample.
+ *
+ * This file adds functions to the Genesis Sample Theme.
+ *
+ * @package Genesis Sample
+ * @author  StudioPress
+ * @license GPL-2.0+
+ * @link    http://www.studiopress.com/
+ */
+
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Setup Theme
+include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
+
+//* Set Localization (do not remove)
+load_child_theme_textdomain( 'genesis-sample', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'genesis-sample' ) );
+
+//* Add Image upload and Color select to WordPress Theme Customizer
+require_once( get_stylesheet_directory() . '/lib/customize.php' );
+
+//* Include Customizer CSS
+include_once( get_stylesheet_directory() . '/lib/output.php' );
+
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', 'Genesis Sample' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_VERSION', '2.2.4' );
+
+//* Enqueue Scripts and Styles
+add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
+function genesis_sample_enqueue_scripts_styles() {
+
+	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Montserrat|Domine', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'dashicons' );
+
+	wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0', true );
+	$output = array(
+		'mainMenu' => __( 'Menu', 'genesis-sample' ),
+		'subMenu'  => __( 'Menu', 'genesis-sample' ),
+	);
+	wp_localize_script( 'genesis-sample-responsive-menu', 'genesisSampleL10n', $output );
+
+}
+
+//* Add HTML5 markup structure
+add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
+
+//* Add Accessibility support
+add_theme_support( 'genesis-accessibility', array( '404-page', 'drop-down-menu', 'headings', 'rems', 'search-form', 'skip-links' ) );
+
+//* Add viewport meta tag for mobile browsers
+add_theme_support( 'genesis-responsive-viewport' );
+
+//* Add support for custom header
+add_theme_support( 'custom-header', array(
+	'width'           => 600,
+	'height'          => 160,
+	'header-selector' => '.site-title a',
+	'header-text'     => false,
+	'flex-height'     => true,
+) );
+
+//* Add support for custom background
+add_theme_support( 'custom-background' );
+
+//* Add support for after entry widget
+add_theme_support( 'genesis-after-entry-widget-area' );
+
+//* Add support for 3-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+//* Add Image Sizes
+add_image_size( 'featured-image', 720, 400, TRUE );
+
+//* Rename primary and secondary navigation menus
+add_theme_support( 'genesis-menus' , array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
+
+//* Reposition the secondary navigation menu
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
+
+//* Reduce the secondary navigation menu to one level depth
+add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
+function genesis_sample_secondary_menu_args( $args ) {
+
+	if ( 'secondary' != $args['theme_location'] ) {
+		return $args;
+	}
+
+	$args['depth'] = 1;
+
+	return $args;
+
+}
+
+//* Modify size of the Gravatar in the author box
+add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
+function genesis_sample_author_box_gravatar( $size ) {
+
+	return 90;
+
+}
+
+//* Modify size of the Gravatar in the entry comments
+add_filter( 'genesis_comment_list_args', 'genesis_sample_comments_gravatar' );
+function genesis_sample_comments_gravatar( $args ) {
+
+	$args['avatar_size'] = 60;
+
+	return $args;
+
+}
+
+/**********************************
+ *
+ * Replace Header Site Title with Inline Logo
+ *
+ * Fixes Genesis bug - when using static front page and blog page (admin reading settings) Home page is <p> tag and Blog page is <h1> tag
+ *
+ * Replaces "is_home" with "is_front_page" to correctly display Home page wit <h1> tag and Blog page with <p> tag
+ *
+ * @author AlphaBlossom / Tony Eppright
+ * @link http://www.alphablossom.com/a-better-wordpress-genesis-responsive-logo-header/
+ *
+ * @edited by Sridhar Katakam
+ * @link http://www.sridharkatakam.com/use-inline-logo-instead-background-image-genesis/
+ *
+************************************/
+add_filter( 'genesis_seo_title', 'custom_header_inline_logo', 10, 3 );
+function custom_header_inline_logo( $title, $inside, $wrap ) {
+
+	$logo = '<img src="' . get_stylesheet_directory_uri() . '/images/MTAH-compact-logo.png" alt="' . esc_attr( get_bloginfo( 'name' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" width="300" height="77" />';
+
+	$inside = sprintf( '<a href="%s" title="%s">%s</a>', trailingslashit( home_url() ), esc_attr( get_bloginfo( 'name' ) ), $logo );
+
+	// Determine which wrapping tags to use - changed is_home to is_front_page to fix Genesis bug
+	$wrap = is_front_page() && 'title' === genesis_get_seo_option( 'home_h1_on' ) ? 'h1' : 'p';
+
+	// A little fallback, in case an SEO plugin is active - changed is_home to is_front_page to fix Genesis bug
+	$wrap = is_front_page() && ! genesis_get_seo_option( 'home_h1_on' ) ? 'h1' : $wrap;
+
+	// And finally, $wrap in h1 if HTML5 & semantic headings enabled
+	$wrap = genesis_html5() && genesis_get_seo_option( 'semantic_headings' ) ? 'h1' : $wrap;
+
+	return sprintf( '<%1$s %2$s>%3$s</%1$s>', $wrap, genesis_attr( 'site-title' ), $inside );
+
+}
+
+// Remove the site description
+remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
+
+
